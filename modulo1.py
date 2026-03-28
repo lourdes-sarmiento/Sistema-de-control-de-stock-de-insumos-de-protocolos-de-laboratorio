1984c4ce70a64a3e8da46c4e01b1165a67fac096
naXoborne <nachito0632@gmail.com>
2026-03-28 18:27:39 -0300

    login

--- modulo1.py
+++ modulo1.py
@@ -1,1 +1,22 @@
+#listas de usuarios y contraseñas para el login
+lista_usuarios = ["carlos" , "javier"]
+lista_contraseñas = ["1" , "2"]
 
+
+def login(usuario_ingresado, contraseña_ingresada, lista_usuarios, lista_contraseñas) :
+    indice = 0
+    while indice <len(lista_usuarios):
+        if lista_usuarios[indice] == usuario_ingresado and lista_contraseñas [indice] == contraseña_ingresada :
+            return True
+        indice = indice + 1
+    return False
+
+
+#programa principal
+usuario_ingresado = input ("ingrese su nombre de usuario: ")
+contraseña_ingresada = input ("ingrese su contraseña: ")
+if login (usuario_ingresado, contraseña_ingresada, lista_usuarios, lista_contraseñas):
+    login_exitoso = True
+    print("bienvenido")
+else:
+    print ("usuario o contraseña incorrectos, intente nuevamente")
